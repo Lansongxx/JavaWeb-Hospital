631d53a249f1d4c6a21ec0e858485d29473c0cfd
zhangye0 <1786236973@qq.com>
2023-07-14 16:10:41 +0800

    zy 修改患者注册bug

--- src/hospital/servlet/PatientRegisterServlet.java
+++ src/hospital/servlet/PatientRegisterServlet.java
@@ -20,10 +20,6 @@ public class PatientRegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String password=req.getParameter("password");
         String SurePassword=req.getParameter("SurePassword");
-        if(!Objects.equals(password, SurePassword)){
-            req.setAttribute("Errormessage","密码不一致，请重新输入");
-            req.getRequestDispatcher("PatientRegister.jsp").forward(req,resp);
-        }
         String name=req.getParameter("name");
         int age= Integer.parseInt(req.getParameter("age"));
         String sex= req.getParameter("sex");
@@ -31,6 +27,14 @@ public class PatientRegisterServlet extends HttpServlet {
         String idCard= req.getParameter("idCard");
         String DateOfBirth= req.getParameter("DateOfBirth");
         String PhoneNumber= req.getParameter("PhoneNumber");
+        if(!Objects.equals(password, SurePassword)){
+            req.setAttribute("Errormessage","密码不一致，请重新输入");
+            req.getRequestDispatcher("PatientRegister.jsp").forward(req,resp);
+        }
+        if (password == null || name == null || sex == null || email == null || idCard == null || DateOfBirth == null || PhoneNumber == null){
+            req.setAttribute("Errormessage","请将所有信息填写完整!");
+            req.getRequestDispatcher("PatientRegister.jsp").forward(req,resp);
+        }
         Patient patient=patientService.PatientRegister(name,password,age,sex,email,idCard,DateOfBirth,PhoneNumber);
         int UserID=patient.getUserID();
 
